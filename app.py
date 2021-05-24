76cebe818b4357bef152cdee58ea973c12c1fc0d
Kavana12345 <57221633+Kavana12345@users.noreply.github.com>
2021-05-24 17:10:00 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from flask import Flask,render_template
+app=Flask(__name__,static_folder='static')
+
+@app.route('/')
+def fun():
+    return render_template("firstpage.html")
+
+if __name__=="__main__":
+    app.run(debug=True)
